69b26418fd69f67803c593e168804a53a4ce1f07
XanMpw <lon25cull01@mpw.ac.uk>
2025-10-21 13:47:57 +0000

    commit 1

--- app.py
+++ app.py
@@ -1,8 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def home():
+    return render_template("home.html")
 
+@app.route("/account")
+def about():
+    return render_template("account.html")
+
+@app.route("/search")
+def search():
+    return render_template("search.html")
+
+@app.route("/watchlists")
+def watchlists():
+    return render_template("watchlists.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
